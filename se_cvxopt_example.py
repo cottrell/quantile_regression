344ab10c9ef20d3a5dd9dc063e2febadd7064bf8
David Cottrell <cottrell@users.noreply.github.com>
2023-01-16 17:24:16 +0000

    Linear programming for quantile linear regression.

--- se_cvxopt_example.py
+++ se_cvxopt_example.py
@@ -1,4 +1,9 @@
 # https://stats.stackexchange.com/questions/384909/formulating-quantile-regression-as-linear-programming-problem/407478#407478
+#
+# NOTE: This is quantile regression for linear regression problem. It turns into a linear programming problem.
+# NOTE: 2023-01-16 I got this working. See do plot.
+#
+
 import io
 
 import numpy as np
@@ -14,9 +19,12 @@ if not os.path.exists(filename):
     base = pd.read_csv(io.StringIO(s.decode('utf-8')), sep='\t')
     base.to_csv(filename)
 
-print(f'reading {filename} ...', end='')
-base = pd.read_csv(filename)
-print(' done')
+try:
+    base
+except NameError:
+    print(f'reading {filename} ...', end='')
+    base = pd.read_csv(filename)
+    print(' done')
 
 tau = 0.3
 
@@ -61,12 +69,55 @@ G[:: n + 1] = -1.0
 # adding inequality constraints - right hand side (all zeros)
 h = matrix(0.0, (n, 1))
 
-# solving the model
-sol = solvers.lp(cm, G, h, Am, bm, solver='glpk')
+def solve_with_cvxopt():
+    # solving the model
+    sol = solvers.lp(cm, G, h, Am, bm, solver='glpk')
 
-x = sol['x']
+    x = sol['x']
 
-# both negative and positive components get values above zero, this gets fixed here
-beta = x[0:K] - x[K : 2 * K]
+    # both negative and positive components get values above zero, this gets fixed here
+    beta = x[0:K] - x[K : 2 * K]
 
-print(beta)
+    print(beta)
+    return locals()
+
+
+from scipy.optimize import linprog
+def solve_with_scipy():
+    sol = linprog(cm, G, h, Am, bm)
+    x = sol['x']
+    # both negative and positive components get values above zero, this gets fixed here
+    beta = x[0:K] - x[K : 2 * K]
+    print(beta)
+    return locals()
+
+
+def plot_solution(beta, beta2=None):
+    import plotly.graph_objects as go
+    x = X.values[:, 1]
+    y = X.values[:, 2]
+    z = b
+    zp = X.values @ beta
+    traces = [
+        go.Scatter3d(z=z, x=x, y=y, marker_size=3, mode='markers', opacity=0.50, marker_color='red', name='data'),
+        go.Scatter3d(z=zp, x=x, y=y, opacity=0.50, mode='markers', marker_size=3, marker_color='blue', name='A'),
+        go.Mesh3d(z=zp, x=x, y=y, opacity=0.50, color='green', name='Surface A'),
+    ]
+    if beta2 is not None:
+        zp2 = X.values @ np.array(beta2).squeeze()
+        traces.extend([
+            go.Scatter3d(z=zp2, x=x, y=y, opacity=0.50, mode='markers', marker_size=3, marker_color='pink', name='B'),
+            go.Mesh3d(z=zp2, x=x, y=y, opacity=0.50, color='purple', name='Surface B'),
+        ])
+        print(f'len traces {len(traces)}')
+
+    fig = go.Figure(data=traces)
+    fig.update_layout(title='quantile regression example', autosize=False, width=500, height=500, margin=dict(l=65, r=50, b=65, t=90))
+    fig.show()
+    return locals()
+
+
+def do():
+    a = solve_with_cvxopt()
+    b = solve_with_cvxopt()
+    plot_solution(a['beta'], b['beta'])
